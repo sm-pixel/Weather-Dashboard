79c94aa72e3b525442c71ad109e9d9824c176c25
sammi morrison <ms.samantha.marie.86@gmail.com>
2020-05-12 20:42:30 -0400

    Added id's and finished html

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// API by city name 
+// api.openweathermap.org/data/2.5/weather?q={city name}&appid=5ae8fdb6f671ad1d9e8649e858708015
+
+//Current city div - city name - name //icon next to city and time - weather.icon
+//temp - main.temp
+//humidity% - main.humidity
+//wind - wind.speed
+//uv index - ?????
