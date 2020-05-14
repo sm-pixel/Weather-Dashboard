87aa86b36b7db0918e4a67849c057f5f8d575751
sammi morrison <ms.samantha.marie.86@gmail.com>
2020-05-13 21:11:08 -0400

    added currentCity function but not working

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,11 +7,12 @@
 //wind - wind.speed
 //uv index - ?????
 
-
+//city
+// let city = $("#searchCity").val().trim();
 // API Key
 const apiKey = "&appid=5ae8fdb6f671ad1d9e8649e858708015";
 // API url
-// var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=&units=imperial&appid=" + city + apiKey;
+// const queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=&units=imperial&appid=" + city + apiKey;
 
 //search city & history
 var searches = JSON.parse(localStorage.getItem("searches")) || [];
@@ -46,11 +47,23 @@ $("form").on("submit", function (event) {
             searchHistory();
         });
 
-    $(document).on("click", ".city", function () {
+    $(document).on("click", ".searchCity", function () {
         console.log($(this).text());
-    });
-})
-    searchHistory();
-//current city display
 
+        currentCity();
+    })
+});
+searchHistory();
+//current city display
+function currentCity() {
+    $("#currentCity").empty()
+
+    $("#currentCity").append($("<h3>").text(response.name))
+    $("#currentCity").append($("<h4>").text(date.toLocaleDateString('en-US')))
+    $("#currentCity").append($("<p>").text("Temp: " + response.main.temp + " °F"))
+    $("#currentCity").append($("<p>").text("Humidity: " + response.main.humidity + "%"))
+    $("#currentCity").append($("<p>").text("Wind Speed: " + response.wind.speed + " MPH"))
+    console.log
+    // $("#CurrentCity").append($("<p>").text("UV Index: " + ???)) also add color???
+}
 //5 day forecast
